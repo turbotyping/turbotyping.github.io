4a54f4445de0b5daa6e5265b7257ae02a639f12f
ahenteti <ahmad.henteti@gmail.com>
2020-11-15 15:39:39 +0100

    add whitespaces

--- src/typescript/components/text-to-type.component.ts
+++ src/typescript/components/text-to-type.component.ts
@@ -7,7 +7,7 @@ const CHARS_To_TYPE: RegExp = /^[A-Za-z0-9é"'\(-è_çà\)=:/;.,?<>!~#{\[|@\]}+ 
 export class TextToTypeHtmlComponent extends BaseHtmlComponent {
 
   private textToTypeDomElement: HTMLElement;
-  private currentCharToTypeDomElement: Element;
+  private currentCharToTypeDomElement: HTMLElement;
   private textToTypeIndex: number = -1;
   private blinkInterval: any;
   private nextCurrentCharToTypeCssClass = 'OK';
@@ -24,15 +24,16 @@ export class TextToTypeHtmlComponent extends BaseHtmlComponent {
 
   private handleKeyDownEvent(event) {
     const char = event.key;
+    const expectedChar = this.currentCharToTypeDomElement.dataset.key;
     if (!CHARS_To_TYPE.test(char)) return;
-    if (this.currentCharToTypeDomElement.textContent !== char) {
+    if (expectedChar !== char) {
       this.nextCurrentCharToTypeCssClass = 'NOK';
       return;
     }
     clearInterval(this.blinkInterval);
     this.currentCharToTypeDomElement.classList.add(this.nextCurrentCharToTypeCssClass);
     this.currentCharToTypeDomElement.classList.remove('cursor');
-    this.currentCharToTypeDomElement = this.currentCharToTypeDomElement.nextElementSibling;
+    this.currentCharToTypeDomElement = this.getNextCharToType();
     this.nextCurrentCharToTypeCssClass = 'OK';
     if (this.currentCharToTypeDomElement) {
       this.currentCharToTypeDomElement.classList.add('cursor');
@@ -43,6 +44,14 @@ export class TextToTypeHtmlComponent extends BaseHtmlComponent {
     }
   }
 
+  private getNextCharToType(): HTMLElement {
+    let res = this.currentCharToTypeDomElement.nextElementSibling;
+    while (res?.tagName === 'WBR') {
+      res = res.nextElementSibling;
+    }
+    return res as HTMLElement;
+  }
+
   private toggleBlinkCssClass() {
     this.textToTypeDomElement.classList.toggle('blink');
   }
@@ -50,11 +59,16 @@ export class TextToTypeHtmlComponent extends BaseHtmlComponent {
   private setNextTextToType(): void {
     this.textToTypeIndex = (this.textToTypeIndex + 1) % englishQuotes.length;
     const textToTypeCharArray = englishQuotes[this.textToTypeIndex].split('');
-    this.textToTypeDomElement.innerHTML = `${textToTypeCharArray.map(c => `<span>${c}</span>`).join('')}`;
+    this.textToTypeDomElement.innerHTML = `${textToTypeCharArray.map(this.charToSpan).join('')}`;
     this.currentCharToTypeDomElement = this.textToTypeDomElement.querySelector('span');
     this.currentCharToTypeDomElement.classList.add('cursor');
     this.textToTypeDomElement.classList.add('blink');
     this.blinkInterval = setInterval(this.toggleBlinkCssClass.bind(this), 350);
+  }
+
+  private charToSpan(c: string) {
+    if (c === ' ') return `<span data-key=" " class="whitespace">␣</span><wbr>`;
+    return `<span data-key="${c}">${c}</span>`;
   }
 }
 
