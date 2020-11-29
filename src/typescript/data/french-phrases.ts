e2d121705472c481d5c0a1d11ae27e0df8e9e159
ahenteti <ahmad.henteti@gmail.com>
2020-11-29 15:11:07 +0100

    translate english quotes

--- src/typescript/data/french-phrases.ts
+++ src/typescript/data/french-phrases.ts
@@ -1,12 +1,302 @@
 export default [
   {
-    quote: 'À vaillant coeur rien d’impossible.',
-    author: 'Jacques Cœur',
-    reference: 'https://takelessons.com/blog/french-quotes-z04',
+    quote:
+      "Prendre ses responsabilités - pratiquer 100% de responsabilité chaque jour - consiste à se voir non pas comme raison ou tort, mais comme un agent, un choix, un résolveur de problèmes et un apprenant dans les interrelations complexes de nos vies afin que nous puissions mieux nous intégrer avec les gens et le monde. autour de nous. Lorsque nous faisons cela, nous profitons d'une manière meilleure et plus productive de vivre et de diriger",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Christopher Avery',
   },
   {
-    quote: 'Dans une grande âme tout est grand.',
-    author: 'Blaise Pascal',
-    reference: 'https://takelessons.com/blog/french-quotes-z04',
+    quote:
+      "La simplicité nécessite un processus en deux étapes. Premièrement, nous devons investir du temps et de l'énergie pour découvrir ce qui nous émeut en tant qu'êtres humains, ce qui fait chanter nos cœurs et ce qui nous apporte de la joie. Ensuite, nous devons continuer à créer la vie qui reflète les personnes uniques que nous sommes vraiment. C'est le cœur et l'âme de la simplicité",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Linda Breen Pierce',
+  },
+  {
+    quote:
+      "Nous avons trois rôles ici sur terre: apprendre, aimer et vivre. Lorsque nous arrêtons d'apprendre, nous commençons à stagner et à mourir. Lorsque nous arrêtons d'aimer, nous perdons notre sens du but et devenons égocentriques. Quand nous limitons notre vie, nous refusons au monde les bénéfices de nos talents",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Jim Cathcart',
+  },
+  {
+    quote:
+      "Un portefeuille vie offre une alternative intéressante à la retraite traditionnelle. C'est une nouvelle façon de penser et de vivre à un âge avancé. Un portfolio typique est un mélange équilibré de travail, d'apprentissage continu, de loisirs, de voyages et d`'avocats, de renouer avec la famille et les amis et de redonner",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Michael Jeans',
+  },
+  {
+    quote:
+      "La plupart de ceux qui ont réussi dans la vie peuvent faire remonter leur succès à l'éducation essentielle qu'ils ont obtenue des parents, des enseignants et / ou des amis",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'T-Ralph Olaniyi',
+  },
+  {
+    quote:
+      "Les fondements de l'honnêteté, du caractère, de la foi, de l'intégrité, de l'amour et de la loyauté sont nécessaires pour un succès équilibré qui comprend la santé, la richesse et le bonheur. Au fur et à mesure que vous avancerez dans la vie, vous découvrirez que si vous compromettez l'un de ces principes, vous vous retrouverez avec seulement une part de mendiant de ce que la vie a à offrir.",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Zig Ziglar',
+  },
+  {
+    quote:
+      "Vous - vous-même, pas quelqu'un d'autre - devez déterminer où vous voulez aller et ce que vous voulez faire dans votre vie. Et une fois que vous avez pris cette décision, vous pouvez commencer à élaborer un plan pour y arriver et à vous concentrer sur cet objectif chaque jour.",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Squire Rushnell',
+  },
+  {
+    quote:
+      "Chaque jour, pensez en vous réveillant, aujourd'hui j'ai la chance d'être en vie, j'ai une vie humaine précieuse, je ne vais pas la gaspiller. Je vais utiliser toutes mes énergies pour me développer, pour étendre mon cœur aux autres; pour atteindre l'illumination pour le bénéfice de tous les êtres. Je vais avoir de bonnes pensées envers les autres, je ne vais pas me fâcher ou penser mal aux autres. Je vais profiter aux autres autant que je peux",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Dalai Lama',
+  },
+  {
+    quote:
+      "La vie est un pari de minute en minute, alors jouez-la aujourd'hui pour tout ce qu'elle contient. Le coup de fouet, la bizarrerie d'un sourire. Une bonne action ici et là en vaut la peine. Alors faites de la vie un jardin pour tous les jours et rassemblez vos boutons de rose pendant que vous le pouvez",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'P. MacDonald',
+  },
+  {
+    quote:
+      "Souvent, dans la vie, ce qui est facile à faire n'en vaut pas la peine. Bien sûr, s'écraser sur le canapé et regarder le dernier épisode de votre émission préférée est une expérience agréable, mais c'est une mauvaise entrée et cela ne vous aidera pas à avancer beaucoup sur la route du succès.",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Amar Hasic',
+  },
+  {
+    quote:
+      'Peu importe à quel point vous pensez que raconter les problèmes qui vous ont causé des problèmes au cours des dernières années, cela ne vous aidera pas à vous diriger vers une vie meilleure ou nouvelle.',
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Catherine Pulsifer',
+  },
+  {
+    quote:
+      "Si vous êtes à la retraite ou si vous commencez à l'attendre, nous avons une bonne nouvelle pour vous: il s'avère que les âges les plus élevés pour le bonheur sont plus tardifs dans la vie",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Patrick Foley',
+  },
+  {
+    quote:
+      'Un ami et moi parlions du contrôle qui accompagne le fait de penser que la vie est comme elle devrait être avec nous. Mais la vérité est que la vie est exactement planifiée',
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Kara Tippetts',
+  },
+  {
+    quote:
+      "Si vous vous efforcez d'obtenir un amour parfait, vous apprendrez à voir le visage de Dieu en votre conjoint, vos enfants et vos semblables. Cela devrait être notre objectif en tant qu'humanité",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Dolores Ayotte',
+  },
+  {
+    quote:
+      'Si vous apprenez à développer une mentalité abondante, vous ne serez pas envieux des autres, vous fêterez leurs succès, vous partagerez leurs joies et leurs peines. Ne voyez pas la vie comme une compétition mais comme complémentaire',
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Bernard Kelvin Clive',
+  },
+  {
+    quote:
+      "Gagner ce n'est pas nécessairement finir premier mais bien finir, au mieux de vos capacités données par Dieu. Lorsque vous adoptez ce concept dans votre vie, vous serez soulagé du stress de la compétition pour finir premier, pour gagner dans la vie",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Dr. Ignatius',
+  },
+  {
+    quote:
+      "Bien qu'il soit vrai que vous avez peu de contrôle sur la plupart de ce qui se passe dans le monde, vous avez une énorme influence sur votre expérience des événements et des circonstances de votre vie.",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Mali Apple',
+  },
+  {
+    quote:
+      "L'un des secrets les plus importants de la vie qui sera le plus satisfait est l'acte de gratitude et d'être reconnaissant pour les choses que vous avez dans votre vie. Il est si naturel de se faire prendre par le besoin continuellement de plus de préparation, que nous ne nous arrêtons pas pour accueillir les choses que nous avons maintenant",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Adam Green',
+  },
+  {
+    quote:
+      'Pour arriver là où nous voulons aller dans la vie, nous devons continuer. Nous devons créer une vision, faire des choix en fonction de ce qui nous pousse le plus rapidement vers nos objectifs et les poursuivre avec détermination et détermination.',
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Stedman Graham',
+  },
+  {
+    quote:
+      'Nos cerveaux ont la capacité et la capacité de continuer à apprendre pendant la majorité de nos vies. Si nous pouvons continuer à avancer, les réponses à la vie se présenteront',
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Phillip Crone',
+  },
+  {
+    quote:
+      "La première étape pour faire un changement dans votre vie, pour obtenir ce que vous voulez de la vie, pour améliorer votre vie est d'abord de décider spécifiquement ce que vous voulez. À moins que vous ne sachiez ce que vous voulez, vous n'arriverez jamais car vous n'avez pas de destination finale",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Catherine Pulsifer',
+  },
+  {
+    quote:
+      "En fin de compte, ce qui compte le plus dans la vie, ce n'est pas combien d'argent vous gagnez, combien de choses matérielles vous acquérez. . . Ce sera la façon dont tu guideras les jeunes que tu apporteras au monde",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Tony Dungy',
+  },
+  {
+    quote:
+      "J'en suis venu à accepter la vie comme un processus, et je suis convaincu que dans mon processus continu, je découvre, m'étend constamment, trouve la cause de mon ignorance, dans les arts martiaux et surtout dans la vie",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Bruce Lee',
+  },
+  {
+    quote:
+      "Que vous le réalisiez ou non, les gens regardent votre vie. Même ceux qui vous rabaissent regardent comment vous vivez. Comment vous vivez votre vie avec ces personnes, c'est comment vous leur montrez l'exemple",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Heidi Kreider',
+  },
+  {
+    quote:
+      "Tout au long de ma vie, j'ai trouvé nécessaire de réorganiser mon emploi du temps pour passer du temps avec ceux que j'aime. Lorsque j'apprécie les gens et que je désire être avec eux, je suis prêt à arrêter tout ce que je fais et à mettre de côté mes projets",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Brian Lake',
+  },
+  {
+    quote:
+      "Il existe plusieurs façons de se déplacer dans notre vie. Nous pouvons nous tenir sur la plage et regarder les vagues rouler, se mouiller à peine les orteils au bord de la ligne de marée; on peut marcher ou courir dans l'eau et plonger dans les vagues",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Barbara Legan',
+  },
+  {
+    quote:
+      "Traiter l'autre personne comme vous aimeriez être traité est une règle d'or qui s'applique non seulement aux rencontres générales de la vie mais aussi aux négociations.",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Jim Berry',
+  },
+  {
+    quote:
+      "Les aventures ont toujours été une partie importante de ma vie ... Elles m'ont donné le courage de surmonter mes peurs, de développer ma confiance en moi et de continuer à grandir en testant mes limites et en les repoussant souvent",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: "Charlotte O'Dea",
+  },
+  {
+    quote:
+      'Une personne sage a dit un jour: "La vie ne vous arrive pas. La vie vous répond." - auteur inconnu. Ceci est absolument vrai. Vous dictez ce que la vie vous donne. Si vous décidez de réussir, il y a de fortes chances que vous soyez',
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Ace McCloud',
+  },
+  {
+    quote:
+      'La passion nous anime tous les jours, comprenez votre passion et comprenez ce que vous voulez dans votre vie car dès que vous commencerez à faire ce que vous aimez le plus, vous commencerez à célébrer le processus',
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Razzab Shaikh',
+  },
+  {
+    quote:
+      "La plupart des gens ne peuvent pas reconnaître la bonne vie quand ils l'ont ... Je crois que la bonne vie, c'est simplement quand vous êtes capable de reconnaître tout le bien dans votre vie - où que ce soit et quand c'est",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Joel Osteen',
+  },
+  {
+    quote:
+      "Pleurnicher et se plaindre de tout dans votre vie. Bruyamment. À chaque occasion donnée. Cela garantira que vous n'obtiendrez jamais rien de mieux, car nous avons tendance à obtenir ce sur quoi nous nous concentrons",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Jack Killian',
+  },
+  {
+    quote:
+      "Avant de recevoir les merveilleux trésors d'une vie heureuse, vous devez d'abord donner. Donnez de vous-même. Soyez au service des autres. Seul ce que vous donnez peut être multiplié dans votre propre vie",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Mary Kay Ash',
+  },
+  {
+    quote:
+      "Beaucoup de gens croient qu'une fois que vous êtes devenu parent, le reste de votre vie devient un exercice sur la façon dont vous pouvez vous concentrer et utiliser les choses et les opportunités qui se présentent.",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Ariel Stefan',
+  },
+  {
+    quote:
+      "Une vie qui semble petite à l'extérieur peut être illimitée à l'intérieur. Même une personne vivant dans l'endroit le plus éloigné peut être préoccupé par des centaines, ou par toute la galaxie",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Obi-Wan Kenobi',
+  },
+  {
+    quote:
+      "L'argent et les choses sont amusants - ils le sont vraiment - mais ils ne peuvent pas soutenir nos esprits. L'argent est un moyen de traverser ce monde, mais il ne peut pas ajouter un jour à nos vies",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Thomas Nelson',
+  },
+  {
+    quote:
+      'Mais en fin de compte, vous êtes responsable de votre propre bien-être et vous vous devez de prendre des décisions qui vous donneront du pouvoir et qui vous aideront à vous améliorer et à améliorer votre vie.',
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'David Bolton',
+  },
+  {
+    quote:
+      "Il n'y a pas de substitut à être soi-même. Dans un monde où tant de gens ne sont pas réels avec eux-mêmes ou avec les autres, je vais jusqu'à dire qu'être authentique est le seul vrai moyen de se démarquer et de gagner dans la vie",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Jeff Davis',
+  },
+  {
+    quote:
+      "J'essaie d'amener les gens à s'intéresser aux particularités de leur vie parce que je pense qu'il y a une chose que la littérature peut faire pour nous. Il peut nous dire: faites attention. Faites plus attention",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Richard Ford',
+  },
+  {
+    quote:
+      "Alors que je traverse toutes sortes de sentiments et d'expériences au cours de mon voyage à travers la vie - joie, surprise, chagrin, consternation - je tiens cette question comme un guide: «De quoi ai-je vraiment besoin maintenant pour être heureux?",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Sharon Salzberg',
+  },
+  {
+    quote:
+      'Il est prouvé que ce que vous voyez et entendez constamment au fil du temps entre dans votre cœur, et ce qui entre dans votre cœur entre dans votre vie et finira par déterminer la qualité de votre vie.',
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Jide Adeniba',
+  },
+  {
+    quote:
+      "Je n'organise ni ne structure la vie simplement parce que c'est mon travail ou parce que je veux que les choses aient l'air bien de l'extérieur. Je fais ces choses parce que je veux une vie riche, douce et simple",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Emily Ley',
+  },
+  {
+    quote:
+      'La plupart des hommes se qualifient de "réparateurs" ou de "résolveurs de problèmes", alors demander à quelqu\'un d\'autre de parler de votre vie et de vous appeler potentiellement sur les angles morts est un peu hors de notre zone de confort.',
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Jerrad Lopes',
+  },
+  {
+    quote:
+      "Pendant leur absence du travail et les tracas de la vie quotidienne, les gens ont tendance à afficher leurs passions et leurs intérêts personnels. Les passe-temps de quelqu'un peuvent en révéler beaucoup sur sa personnalité",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'David S. Walton',
+  },
+  {
+    quote:
+      "L'époque des grandes entreprises qui s'occupent de nous du début à la fin est révolue. Mais ce sont des nouvelles passionnantes. Cela signifie que nous pouvons choisir la vie que nous voulons pour nous-mêmes",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Dick Costolo',
+  },
+  {
+    quote:
+      "Au fil des ans, surtout rétrospectivement, j'ai appris une variété de leçons, et l'une des plus importantes est la suivante: vous ne pouvez pas atteindre une cible que vous ne pouvez pas voir. Vous ne pouvez pas accomplir des choses merveilleuses de votre vie si vous n'avez aucune idée de ce qu'elles sont. Vous devez d'abord devenir absolument clair sur ce que vous voulez si vous voulez vraiment libérer le pouvoir extraordinaire qui se trouve en vous.",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Brian Tracy',
+  },
+  {
+    quote:
+      "Tout ce qui rend notre vie quotidienne plus facile, plus productive, plus agréable et plus agréable a été créé à cause d'un problème, et parce qu'un designer ou une équipe de designers quelque part dans le monde a cherché à résoudre ce problème",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Bill Burnett',
+  },
+  {
+    quote:
+      "Si vous trouvez que vous avez très peu de temps pour vous engager dans la création de votre propre destin, ou pour votre développement personnel, ou pour dépenser avec votre famille et vos amis, ou pour consacrer une partie à aider d'autres personnes dans le besoin, alors vous voudrez peut-être revoir vos priorités. dans la vie",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Robert Rivers',
+  },
+  {
+    quote:
+      "De nombreuses personnes choisissent d'être généreuses envers les autres et de leur donner du pouvoir lorsque des opportunités se présentent. Cela devient pour eux un mantra, un mode de vie, une philosophie et une doctrine, un compagnon de vie et une collection d'instincts autorégulateurs qui se gravent dans leur subconscient.",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Charles C. Malone',
+  },
+  {
+    quote:
+      "Les dilemmes moraux sont une caractéristique omniprésente dans la vie organisationnelle, et la discipline de l'éthique offre des principes, des outils et des concepts pour les analyser et prendre une décision sur ce qu'il faut faire",
+    reference: 'https://www.wow4u.com/lifeq2/',
+    author: 'Oyvind Kvalnes',
   },
 ];
