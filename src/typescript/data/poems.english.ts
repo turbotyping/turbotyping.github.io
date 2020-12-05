004df9ede5462a06c30b7ec7b6ad2bbcffd9f0c8
ahenteti <ahmad.henteti@gmail.com>
2020-12-05 15:15:19 +0100

    add more poems

--- src/typescript/data/poems.english.ts
+++ src/typescript/data/poems.english.ts
@@ -5,4 +5,28 @@ export default [
     author: 'Catherine Pulsifer',
     reference: 'https://www.wow4u.com/poemofhope/',
   },
+  {
+    text:
+      'Life is an opportunity, seize the day, live each day to the fullest.\nLife is not a project, but a journey to be enjoyed.\n\nLife is a gift, accept it, enjoy it, and be happy with it.\nLife is a challenge, but challenges can always be overcome.\n\nLife is to be lived, not to be dreamed.\nLife is a wonder, if you sit back and be thankful.\n\nLife is a decision each of us makes, to be happy or sad.\nLife is a time for new beginnings and endings.\n\nLife is a balance, take time for play not just work.\nLife is a joy for those who are optimistic.\n\nLife is a bundle of possibilities if you just look for them.\nLife is a meant to do, not to regret.\nLife is no bed of roses but life is what you make it.',
+    author: 'Catherine Pulsifer',
+    reference: 'https://www.wow4u.com/poemofhope/',
+  },
+  {
+    text:
+      "Life is no bed of roses\nFor roses have thorns, you see,\nBut handling with care and balance\nWill protect from injury.\n\nAll gardens have seasons of beauty\nWhere the flowers of our living grow\nWith colours and shapes aplenty\nSweet fragrance to bestow.\n\nLife's flowers need careful tending\nFor all life needs sun and rain,\nAnd Love and Faith unending\nFor Hope to flower again!",
+    author: 'John McLeod',
+    reference: 'https://www.wow4u.com/poemofhope/',
+  },
+  {
+    text:
+      "Do not forget to hope\nThough Time spins fleeting on\nThere is a place for all\nTo view the promised dawn\nWhen Life's full test have run\nLike rivers to the sea\nDo not forget to hope\nAnd happy be.\n\nDo not forget to hope\nFor even when one's life\nSeems burdened down with care\nAnd melancholy strife\nRemember how the gulls\nSoar beautiful and free?\nDo not forget to hope\nAnd happy be.\n\nDo not forget to hope\nNor rise above the gloom\nAll colours, as they say,\nAre needed on Life's Loom,\nAnd 'dark before the dawn'\nIs truth that all may see?\nDo not forget to hope\nAnd happy be.",
+    author: 'John McLeod',
+    reference: 'https://www.wow4u.com/poemofhope/',
+  },
+  {
+    text:
+      'Hope keeps us going\neven when the answers are not knowing.\nNo matter what is going on\nHope can let us look forward to the new dawn.\n\nThe saying "this too shall pass"\nGives us hope in mass.\nLife is full of ups and downs\nWe must keep hope and not frown.\n\nHope for better days\nIs a good prayer to pray.\nFocus on your blessings\nDon\'t just sit and be stressing.\n\nNever lose your hope\nHoping and action lets you cope.\nHaving hope helps live move on\nBy focusing on the good, your problems will be gone.',
+    author: 'Kate Summers',
+    reference: 'https://www.wow4u.com/poemofhope/',
+  },
 ];
--- src/typescript/data/poems.french.ts
+++ src/typescript/data/poems.french.ts
@@ -5,4 +5,28 @@ export default [
     author: 'Catherine Pulsifer',
     reference: 'https://www.wow4u.com/poemofhope/',
   },
+  {
+    text:
+      "La vie est une opportunité, saisissez la journée, vivez chaque jour au maximum.\nLa vie n'est pas un projet, mais un voyage à apprécier.\n\nLa vie est un cadeau, acceptez-le, profitez-en et soyez heureux avec.\nLa vie est un défi, mais les défis peuvent toujours être surmontés.\n\nLa vie est à vivre, non à rêver.\nLa vie est une merveille, si vous vous asseyez et soyez reconnaissant.\n\nLa vie est une décision que chacun de nous prend, être heureux ou triste.\nLa vie est un temps pour de nouveaux commencements et fins.\n\nLa vie est un équilibre, prenez le temps de jouer, pas seulement de travailler.\nLa vie est une joie pour ceux qui sont optimistes.\n\nLa vie est un ensemble de possibilités si vous les recherchez simplement.\nLa vie est censée faire, ne pas regretter.\nLa vie n'est pas un lit de roses mais la vie est ce que vous en faites.",
+    author: 'Catherine Pulsifer',
+    reference: 'https://www.wow4u.com/poemofhope/',
+  },
+  {
+    text:
+      "La vie n'est pas un lit de roses\nCar les roses ont des épines, tu vois,\nMais manipuler avec soin et équilibre\nProtègera des blessures.\n\nTous les jardins ont des saisons de beauté\nOù poussent les fleurs de notre vie\nAvec des couleurs et des formes en abondance\nParfum doux à donner.\n\nLes fleurs de la vie ont besoin d'être soignées\nCar toute vie a besoin de soleil et de pluie,\nEt l'amour et la foi sans fin\nPour l'espoir de fleurir à nouveau!",
+    author: 'John McLeod',
+    reference: 'https://www.wow4u.com/poemofhope/',
+  },
+  {
+    text:
+      "N'oublie pas d'espérer\nBien que le temps passe vite\nIl y a une place pour tous\nPour voir l'aube promise\nQuand le test complet de la vie a été exécuté\nComme les rivières à la mer\nN'oublie pas d'espérer\nEt heureux être.\n\nN'oublie pas d'espérer\nCar même quand sa vie\nSemble chargé de soins\nEt des conflits mélancoliques\nRappelez-vous comment les mouettes\nEnvolez-vous beau et libre?\nN'oublie pas d'espérer\nEt heureux être.\n\nN'oublie pas d'espérer\nNi s'élever au-dessus de l'obscurité\nToutes les couleurs, comme on dit,\nSont nécessaires sur Life's Loom,\nEt 'sombre avant l'aube'\nLa vérité est-elle que tout le monde peut voir?\nN'oublie pas d'espérer\nEt heureux être.",
+    author: 'John McLeod',
+    reference: 'https://www.wow4u.com/poemofhope/',
+  },
+  {
+    text:
+      "L'espoir nous fait avancer\nmême quand les réponses ne savent pas.\nPeu importe ce qui se passe\nL'espoir peut nous laisser espérer la nouvelle aube.\n\nLe dicton \"cela aussi passera\"\nNous donne de l'espoir en masse.\nLa vie est pleine de hauts et de bas\nNous devons garder espoir et ne pas froncer les sourcils.\n\nJ'espère des jours meilleurs\nEst une bonne prière pour prier.\nConcentrez-vous sur vos bénédictions\nNe vous contentez pas de vous asseoir et de vous stresser.\n\nNe perds jamais ton espoir\nL'espoir et l'action vous permettent de faire face.\nAvoir de l'espoir aide à vivre, à avancer\nEn vous concentrant sur le bien, vos problèmes auront disparu.",
+    author: 'Kate Summers',
+    reference: 'https://www.wow4u.com/poemofhope/',
+  },
 ];
