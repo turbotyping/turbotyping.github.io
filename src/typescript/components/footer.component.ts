e0555b55850e181a05b590f7fd1ac9830a497f2b
ahenteti <ahmad.henteti@gmail.com>
2020-12-10 00:15:30 +0100

    update website footer

--- src/typescript/components/footer.component.ts
+++ src/typescript/components/footer.component.ts
@@ -4,8 +4,8 @@ export class FooterHtmlComponent extends BaseStaticBlockHtmlComponent {
   __toHtml() {
     return /* html */ `
       <footer>
-        <p>This website was mainly inspired from <a href="https://www.keybr.com/" target="_blank">keybr</a> website
-        <p>It lets users type <b>Quotes</b> and <b>Poems</b> while improving their typing skills :)</p>
+        <p>This website was mainly inspired from <b><a href="https://www.keybr.com/" target="_blank">keybr</a></b> website
+        <p>It lets users type <b>Codes</b>, <b>Quotes</b>, <b>Poems</b>, ... while improving their typing skills :)</p>
         <p class="copyright">© 2020 devkeyboard.github.io</p>
       </footer>
     `;
--- src/typescript/constants/constant.ts
+++ src/typescript/constants/constant.ts
@@ -1,4 +1,4 @@
-export const APP_STORAGE_LOCAL_STORAGE_KEY = 'app-storage-local-storage-key';
+export const APP_STORAGE_LOCAL_STORAGE_KEY = 'app-storage-local-storage-key-v2';
 export const PROGRESS_DIV_ID = 'ProgressDivId';
 export const MIN_STATS_TO_DISPLAY = 5;
 export const LIGHT_THEME_VALUE = 'light';
