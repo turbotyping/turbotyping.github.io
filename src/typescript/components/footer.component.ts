d567193b16d8302b9023d684f4a7d7bd32cc1d15
ahenteti <ahmad.henteti@gmail.com>
2020-12-12 15:41:41 +0100

    improve website footer

--- src/typescript/components/footer.component.ts
+++ src/typescript/components/footer.component.ts
@@ -4,8 +4,9 @@ export class FooterHtmlComponent extends BaseStaticBlockHtmlComponent {
   __toHtml() {
     return /* html */ `
       <footer>
-        <p>This website was mainly inspired from <b><a href="https://www.keybr.com/" target="_blank">keybr</a></b> website
-        <p>It lets users type <b>Codes</b>, <b>Quotes</b>, <b>Poems</b>, ... while improving their typing skills :)</p>
+        <p>This website was mainly inspired from <b><a href="https://www.keybr.com/" target="_blank">keybr</a></b> website, really great job!</p>
+        <p>What we can find in our website is the ability to write <b>Codes</b>, <b>Quotes</b>, <b>Poems</b>, ... while improving your typing skills 🙂</p>
+        <p>For any suggestion, please feel free to create a <b><a href="https://github.com/devkeyboard/devkeyboard.github.io/issues/new" target="_blank">ticket</a></b> in our github repository and we will try to add it ASAP</p>
         <p class="copyright">© 2020 devkeyboard.github.io</p>
       </footer>
     `;
