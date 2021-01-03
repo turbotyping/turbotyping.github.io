1d429b1acd64d1d9a62398daf4121cbdef390297
ahenteti <ahmad.henteti@gmail.com>
2021-01-03 19:25:44 +0100

    add website header

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { FooterHtmlComponent } from './components/footer/footer.component';
 import { NavbarHtmlComponent } from './components/navbar/navbar.component';
 import { IHtmlComponent } from './components/_core/component.interface';
 
-const body = document.querySelector('body');
+const main = document.querySelector('main');
 
 const components: IHtmlComponent[] = [];
 components.push(new NavbarHtmlComponent());
@@ -13,5 +13,5 @@ components.push(new MainHtmlComponent());
 components.push(new FooterHtmlComponent());
 
 components.forEach((component) => component.preInsertHtml());
-components.forEach((component) => component.insertHtml(body, 'beforeend'));
+components.forEach((component) => component.insertHtml(main, 'beforeend'));
 components.forEach((component) => component.postInsertHtml());
