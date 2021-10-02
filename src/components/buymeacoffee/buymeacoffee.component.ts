cf916e1f5d326ac5e3eb1f8da558500e1f048a03
ahenteti <ahmad.henteti@gmail.com>
2021-10-02 22:07:55 +0200

    improve app-setting-side-panel styles

---
+++ src/components/buymeacoffee/buymeacoffee.component.ts
@@ -0,0 +1,14 @@
+import './buymeacoffee.scss';
+import { BaseHtmlComponent } from '../_core/base-component';
+
+export class BuyMeACoffeeComponent extends BaseHtmlComponent {
+  toHtml(): string {
+    return /* html */ `
+      
+    `;
+  }
+
+  preInsertHtml(): void {}
+
+  postInsertHtml(): void {}
+}
