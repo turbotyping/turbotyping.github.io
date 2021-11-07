65f18da935ebd9ac99dace6a73b7bf5eabee8932
ahenteti <ahmad.henteti@gmail.com>
2021-11-07 02:05:27 +0100

    improve dark theme

--- src/components/how-we-use-this-application/how-we-use-this-application.component.ts
+++ src/components/how-we-use-this-application/how-we-use-this-application.component.ts
@@ -1,9 +1,19 @@
 import './how-we-use-this-application.scss';
 import { BaseHtmlComponent } from '../_core/base-component';
+import { CHANGE_THEME_EVENT, LIGHT_THEME_VALUE } from '../../constants/constant';
+import { IAppStateClient } from '../../state/app-state.client.interface';
+import { AppStateClient } from '../../state/app-state.client';
 
 export class HowWeUseThisApplicationHtmlComponent extends BaseHtmlComponent {
+  private keyboardId: string;
+  private keyboard: HTMLImageElement;
+
+  constructor(private appStateClient: IAppStateClient = AppStateClient.getInstance()) {
+    super();
+  }
+
   preInsertHtml(): void {
-    // nothing to do
+    this.keyboardId = this.generateId();
   }
 
   toHtml() {
@@ -23,13 +33,24 @@ export class HowWeUseThisApplicationHtmlComponent extends BaseHtmlComponent {
           <b>F</b> and <b>J</b> keys and you should put your index fingers on the bumps. Each finger is responsible for its own 
           set of keys, as explained in the following illustration:
         </p>
-        <img style="margin: 2rem auto 0; display: flex" src="home-keys-position.svg" />
+        <img id="${this.keyboardId}" style="margin: 2rem auto 0; display: flex" />
 
       </div>
     `;
   }
 
   postInsertHtml(): void {
-    // nothing to do
+    this.keyboard = document.getElementById(this.keyboardId) as HTMLImageElement;
+    this.setKeyboardImageSource();
+    this.addCustomEventListener(CHANGE_THEME_EVENT, this.setKeyboardImageSource.bind(this));
+  }
+
+  private setKeyboardImageSource() {
+    const theme = this.appStateClient.getAppState().currentTheme;
+    if (theme === LIGHT_THEME_VALUE) {
+      this.keyboard.src = 'home-keys-position-light.svg';
+    } else {
+      this.keyboard.src = 'home-keys-position-dark.svg';
+    }
   }
 }
